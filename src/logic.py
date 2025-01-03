d0a4df216651a42e1d1fd085f1e7e41355771bd2
Vokymir <voky.consultant@gmail.com>
2025-01-03 23:16:06 +0100

    delete group

--- src/logic.py
+++ src/logic.py
@@ -1,12 +1,26 @@
+import os
+
+from ui import UI
+from group import Group
+from helper import GroupError
+
+
 class Logic:
     basefolder: str
     intervals: list[int]
+    ui: UI
+    groups: list[Group]
 
     def __init__(self, basefolder: str, intervals: list[int]):
         self.basefolder = basefolder
         self.intervals = intervals
+        self.ui = UI()
 
     def parse_args(self, args: list[str]) -> bool:
+        """
+        Parse all arguments and do what they say.
+        Return True on success, False on failure.
+        """
         should_compile: bool = False
         should_run: bool = False
         should_add_qna: bool = False
@@ -46,12 +60,19 @@ class Logic:
 
                 case "-ge":
                     should_edit_group = True
+                    break
 
                 case "-d":
                     should_del_qna = True
+                    break
 
                 case "-gd":
                     should_del_group = True
+                    break
+
+                case _:
+                    self.ui.how_to_use()
+                    return False
 
         if should_del_group:
             self.del_group()
@@ -69,6 +90,40 @@ class Logic:
             self.compile()
         if should_run:
             self.run_qnas()
+
+        return True
+
+    def load_groups(self) -> bool:
+        self.groups = list()
+
+        for filename in os.listdir(self.basefolder):
+            # exclude other files
+            if filename.split(".")[1] != "qna":
+                continue
+            # load group
+            g: Group = Group(self.basefolder)
+            # check if valid
+            if not g.load_group(filename.split(".")[0]):
+                return False
+            # add to arr
+            self.groups.append(g)
+
+        return True
+
+    def del_group(self) -> bool:
+        # make sure groups are complete
+        if len(self.groups) == 0:
+            self.load_groups()
+        # ask for group
+        group_id = self.ui.which_group(self.groups)
+        # delete group and check
+        g = Group(self.basefolder)
+        if g.load_group(group_id) != GroupError.SUCCESS:
+            return False
+        if g.delete_group() != GroupError.SUCCESS:
+            return False
+        # all done
+        return True
 
     def compile(self) -> bool:
         """
--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,33 @@
+from group import Group
+
+
 class UI:
     basefolder: str
 
-    def __init__(self, basefolder: str):
-        self.basefolder = basefolder
+    def __init__(self):
+        pass
+
+    def how_to_use(self):
+        pass
+
+    def which_group(self, groups: list[Group]) -> str:
+        """
+        Show user all groups and let him choose one.
+        Return group ID.
+        """
+        string: str = ""
+        res
+
+        for i in range(len(groups)):
+            string += f"({i}) {groups[i].g_name} | "
+
+        while 1:
+            print(string)
+            res = input("Plese select group, by entering assigned number:", end=" ")
+            try:
+                res = int(res)
+                break
+            except:
+                continue
+
+        return groups[res].g_id
